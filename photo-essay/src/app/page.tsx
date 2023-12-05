b704ff80985ed6b87fb2c233ef39a8ff02255e37
Dominguez <pilymdm@bu.edu>
2023-12-04 21:43:19 -0600

    feat: finish first draft

--- photo-essay/src/app/page.tsx
+++ photo-essay/src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/HeroSection"
+import MainContent from "./components/MainContent"
 
 export default function Home() {
   return (
     <div className="bg-white">
       <HeroSection/>
+      <MainContent/>
     </div>
   )
 }
